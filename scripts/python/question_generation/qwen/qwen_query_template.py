5356b0ec6bc8b5f568ddfd5c6dd21a5ce17182b9
Christopher Pukas <christopher.pukas@gmail.com>
2026-05-28 12:30:41 +0100

    Fixed question generation

---
+++ scripts/python/question_generation/qwen/qwen_query_template.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import llm_benchmark.config as config
+
+from typing import Dict
+
+from llm_benchmark.utils.llm_interface.models.local.qwen import QwenInterfaceModule
+from llm_benchmark.utils.llm_interface.generation_utils import savepath_formatting
+from llm_benchmark.utils.benchmark import generate
+
+def query(model_name: str = "Qwen/Qwen-7B-Chat"):
+    defaults: Dict[str, str] = {
+        "unhydrated_question_save_path": "/rds/general/user/cp824/home/neurips_llms/llm-benchmark/db/generation/",
+    }
+
+    parser = argparse.ArgumentParser(description="Query Qwen Model")
+    parser.add_argument("--model_name", type=str, default=model_name, help="Name of the Qwen model to use")
+    parser.add_argument("--save_path", type=str, default=None, help="Path to save the generated questions")
+    parser.add_argument("--cache_dir", type=str, default="/rds/general/user/cp824/home/neurips_llms/llm-benchmark/db/seshat", help="Path to the cache directory")
+    args = parser.parse_args()
+
+    final_save_path: str = savepath_formatting(model_name, defaults.get("unhydrated_question_save_path")) if args.save_path is None else args.save_path
+    os.makedirs(final_save_path, exist_ok=True)
+
+    question_instance: QwenInterfaceModule = QwenInterfaceModule(
+        model_name=args.model_name,
+        local = True,
+        trust_remote_code=True
+    )
+
+    generate(
+        save_path=final_save_path,
+        LLMInterfaceModule=question_instance,
+        seshat_cache_dir=args.cache_dir,
+        polity_mapping=config.polity_mapping,
+        polities_to_evaluate=["wf"]
+    )
