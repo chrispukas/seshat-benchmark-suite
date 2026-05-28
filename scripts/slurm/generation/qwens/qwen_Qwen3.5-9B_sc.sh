8a655baf40cc0993e0840aeaf14816bc6612b11a
Christopher Pukas <christopher.pukas@gmail.com>
2026-05-28 12:42:38 +0100

    Slurm jobs, and commits

---
+++ scripts/slurm/generation/qwens/qwen_Qwen3.5-9B_sc.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#PBS -l walltime=08:00:00
+#PBS -l select=1:ncpus=8:mem=32gb:ngpus=1
+#PBS -N q3.5_9B+SC
+
+LOCAL_DIR=/rds/general/user/cp824/home
+REPO_ROOT="${LOCAL_DIR}/neurips_llms/llm-benchmark"
+
+echo "Activating conda environment"
+source "${LOCAL_DIR}/miniforge3/etc/profile.d/conda.sh"
+conda activate qwen
+
+export PYTHONPATH="${REPO_ROOT}:${PYTHONPATH}"
+
+echo "Checking Python path:"
+which python
+python -c "import sys; print('\n'.join(sys.path))"
+
+echo "Running Qwen query script"
+cd "${REPO_ROOT}/scripts/python/question_generation/qwen"
+python "qwen_Qwen3.5-9B_sc.py"
